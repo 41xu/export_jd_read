7974dc4a59d7171ea7e4f34772bcf125d4738460
41xu <xsy9915@gmail.com>
2023-05-19 17:52:39 +0800

    update html2pdf, merge pdf

--- html2pdf.py
+++ html2pdf.py
@@ -1,4 +1,12 @@
 import pdfkit
+from PyPDF2 import PdfMerger
 
-for i in range(16, 37):
-    pdfkit.from_file(f'./output/30440345/1470968{i}.html', f'./output/{i-16}.pdf')
+# for i in range(16, 37):
+#     pdfkit.from_file(f'./output/30440345/1470968{i}.html', f'./output/{i-16}.pdf')
+
+pdfs = [f'./output/{i}.pdf' for i in range(0, 21)]
+file_merger = PdfMerger()
+for pdf in pdfs:
+    file_merger.append(pdf)
+
+file_merger.write('./output/total.pdf')
